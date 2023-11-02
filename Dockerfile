ddcef485df6615a66eb9549d096bc63ffa3ebfff
Rishit Desai <rishit-desai@users.noreply.github.com>
2023-11-03 00:41:41 +0530

    final commit

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:14-alpine
 WORKDIR /src
-COPY package*.json app.js ./
+COPY package*.json app.js index.html ./
 RUN npm install
 EXPOSE 8080
 CMD ["node", "app.js"]
--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+
+app.get('/name', (req, res) =>
+{
+    let name = req.query.name
+    res.send(`<h1>Hello ${name}</h1>`)
+});
+
+app.get('/current-year', (req, res) =>
+{
+    let year = new Date().getFullYear()
+    res.send(String(year))
+})
+
 try
 {
-    app.listen(8080, () => console.log('Server ready'))
+    app.listen(8080, () => console.log('Server ready'));
 }
 catch (error)
 {
-    console.log(error)
+    console.log(error);
 }
